29a76a9b884fa6816e4d6defdafda635e8030658
Thomas Coats <thomas.coats@gmail.com>
2025-07-08 23:46:40 +1200

    Added mysql2

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import sleep from 'xy-api/sleep.js'
 
 const loop = async () => {
-  while (true) {
-    await sleep(1000)
-  }
+  while (true) await sleep(1000)
 }
 console.log('No implementation...')
 loop()
